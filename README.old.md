db4fbda77f34aa51aa46db78ddc52136117fff68
Devaka Dias <87965434+Wazoli9@users.noreply.github.com>
2022-04-09 15:57:55 +0530

    setup firebase connection

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# villvay_todo
+ 
---
+++ src/firebase_config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJx6NnMIRP9jsF0xZnb22NtRm135M_PD0",
+    authDomain: "villvay-todo.firebaseapp.com",
+    projectId: "villvay-todo",
+    storageBucket: "villvay-todo.appspot.com",
+    messagingSenderId: "908426692342",
+    appId: "1:908426692342:web:942b3ae2f420e3d37e4ed7",
+};
+
+const app = initializeApp(firebaseConfig);
