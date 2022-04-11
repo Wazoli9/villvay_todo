e771badbe697f041383f463849e78a0511ebdc76
Devaka Dias <87965434+Wazoli9@users.noreply.github.com>
2022-04-11 13:46:33 +0530

    foramtted dates dd-mm-yyyy

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 export default function Todo(props) {
+    const formatDate = () => {
+        if (props.todo.date) {
+            const dateArray = props.todo.date.split("-");
+            return `${dateArray[2]}-${dateArray[1]}-${dateArray[0]}`;
+        }
+    };
     return (
         <div className="todo">
             <div className="todo-info">
@@ -11,17 +17,39 @@ export default function Todo(props) {
                         <h3 className="todo-asignee">{props.todo.asignee}</h3>
                     </div>
                     <div className="todo-detail">
-                        <h4>Date</h4>
-                        <h3 className="todo-date">{props.todo.date}</h3>
+                        <h4>Due Date</h4>
+                        <h3 className="todo-date">{formatDate()}</h3>
                     </div>
                     <div className="todo-detail">
-                    <h3 onClick={()=>props.handleDoneToggle(props.id, props.todo.done)} className="todo-btn btn">{props.todo.done ? "Not Done" : "Done"}</h3>
+                        <h3
+                            onClick={() =>
+                                props.handleDoneToggle(
+                                    props.id,
+                                    props.todo.done
+                                )
+                            }
+                            className="todo-btn btn"
+                        >
+                            {props.todo.done ? "Not Done" : "Done"}
+                        </h3>
                     </div>
                     <div className="todo-detail">
-                    <h3 onClick={()=>props.handleEdit(props.id, props.todo)} className="todo-btn btn">Edit</h3>
+                        <h3
+                            onClick={() =>
+                                props.handleEdit(props.id, props.todo)
+                            }
+                            className="todo-btn btn"
+                        >
+                            Edit
+                        </h3>
                     </div>
                     <div className="todo-detail">
-                    <h3 onClick={()=>props.handleDelete(props.id)} className="todo-btn btn">Delete</h3>
+                        <h3
+                            onClick={() => props.handleDelete(props.id)}
+                            className="todo-btn btn"
+                        >
+                            Delete
+                        </h3>
                     </div>
                 </div>
             </div>
